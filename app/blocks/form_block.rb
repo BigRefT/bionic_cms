931c8b1ee770960bbd8b694575dac03ea417e417
BigRefT <tonyfontenot@gmail.com>
2010-09-23 13:30:11 -0400

    parse the action through context before creating the form

--- app/blocks/form_block.rb
+++ app/blocks/form_block.rb
@@ -5,11 +5,22 @@ class FormBlock < Liquid::Block
   end
 
   def render(context)
-    rvalue = "<form method=\"post\" action=#{@form_action}>"
-    rvalue += "<div style=\"margin: 0pt; padding: 0pt;\"><input type=\"hidden\" value=\"#{context.registers['form_authenticity_token']}\" name=\"authenticity_token\"/></div>"
+    @context = context
+    rvalue = "<form method=\"post\" action=\"#{parse_attribute(@form_action)}\">"
+    rvalue += "<div style=\"margin: 0pt; padding: 0pt;\"><input type=\"hidden\" value=\"#{@context.registers['form_authenticity_token']}\" name=\"authenticity_token\"/></div>"
     super.each { |a| rvalue += a }
     rvalue += "</form>"
     rvalue
+  end
+
+private
+
+  def parse_attribute(value)
+    if value =~ Bionic::QuotedAttribute
+      $1 || $2
+    else
+      @context[value] || value
+    end
   end
 end
 Liquid::Template.register_tag('form', FormBlock)
