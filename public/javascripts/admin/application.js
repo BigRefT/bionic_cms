95ad5bde3ff0db953e3654778527d393f7132559
BigRefT <tonyfontenot@gmail.com>
2010-09-27 16:49:18 -0400

    add pagination ajax javascript

--- public/javascripts/admin/application.js
+++ public/javascripts/admin/application.js
@@ -32,6 +32,29 @@ function AddClasses(navRoot)
   }
 }
 
+/*-------------- pagination ajax ---------------*/
+document.observe("dom:loaded", function() {
+  // the element in which we will observe all clicks and capture
+  // ones originating from pagination links
+  var container = $(document.body)
+
+  if (container) {
+
+    function createSpinner() {
+      return new Element('img', { src: '/images/admin/icons/loading.gif', 'class': 'spinner' })
+    }
+
+    container.observe('click', function(e) {
+      var el = e.element()
+      if (el.match('.pagination.ajax a')) {
+        el.up('.pagination.ajax').insert(createSpinner())
+        new Ajax.Request(el.href, { method: 'get' })
+        e.stop()
+      }
+    })
+  }
+})
+
 /*-------------------- Product Administration Functions ------------------------------*/
 var Product = {
   product_id: null,
